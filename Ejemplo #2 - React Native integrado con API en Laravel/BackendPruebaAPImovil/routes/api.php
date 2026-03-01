33edaf86f43fb6779ad8f5391c511fd81d759108
Cristian Andres Arenas Vargas <crarenasv@unal.edu.co>
2026-03-01 00:02:18 -0500

    Ejemplo

--- Ejemplo #2 - React Native integrado con API en Laravel/BackendPruebaAPImovil/routes/api.php
+++ Ejemplo #2 - React Native integrado con API en Laravel/BackendPruebaAPImovil/routes/api.php
@@ -4,6 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExampleController;
 
-Route::get('/examples', [ExampleController::class, 'index']);
-Route::post('/examples', [ExampleController::class, 'store']);
-Route::get('/examples/{id}', [ExampleController::class, 'show']);
+    Route::get('/examples', [ExampleController::class, 'index']);
+    Route::post('/examples', [ExampleController::class, 'store']);
+    Route::get('/examples/{id}', [ExampleController::class, 'show']);
